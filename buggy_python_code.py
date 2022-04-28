278d928fe087fe557bbcebc57d00bab9187fac63
player235 <100539532+player235@users.noreply.github.com>
2022-04-28 10:45:48 +0200

    small fixes

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,19 +1,20 @@
 # contains bunch of buggy examples
-# taken from https://hackernoon.com/10-common-security-gotchas-in-python-and-how-to-avoid-them-e19fbe265e03
-import cPickle
-import subprocess
+# taken from 
+# https://hackernoon.com/10-common-security-gotchas-in-python-and-how-to-avoid-them-e19fbe265e03
 import base64
 import subprocess
 import flask
 
 # Input injection
-def transcode_file(request, filename):
+
+
+def transcode_file(_, filename):
     command = 'ffmpeg -i "{source}" output_file.mpg'.format(source=filename)
     subprocess.call(command, shell=True)  # a bad idea!
 
 
 # Assert statements
-def foo(request, user):
+def foo(_, user):
     assert user.is_admin, 'user does not have access'
     # secure code...
 
@@ -23,8 +24,10 @@ class RunBinSh(object):
     def __reduce__(self):
         return (subprocess.Popen, (('/bin/sh',),))
 
+
 def import_urlib_version(version):
     exec("import urllib%s as urllib" % version)
+
 
 @app.route('/')
 def index():
